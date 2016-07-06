41989e30c689f0c8e22328be24017e3cac6acea5
RomanovArtem <temik_romanov@mail.ru>
2016-07-06 15:12:28 +0400

    При на ведении на пункт меню, плавно выпадает подменю. (Можно сделать по щелчку).

--- js/nav-menu.js
+++ js/nav-menu.js
@@ -1,17 +1,20 @@
 $(document).ready(function() {
 
-	$('#side-bar > ul > li > a').click(function(){
+	$('#side-bar > ul > li > a').mouseover(function(){
     
-       // alert("Рад видеть Вас на моем сайте! Пошли дальше?");
-        // if($(this).attr('class') != 'active'){
+       // alert("!!!!!!!!!!");
+         if($(this).attr('class') != 'active')
+         {
         $('#side-bar > ul > li > ul').slideDown(400);
-        //$(this).next().slideToggle(400); 
-        //$('#side-bar > ul > li > a').removeClass('active');
-        //$(this).addClass('active');}
-       // else{
-        //    $('#side-bar > ul > li > a').removeClass('active');
-        //    $('#side-bar > ul > li > ul').slideDown(400);
-       // }
+        $(this).next().slideToggle(400); 
+        $('#side-bar > ul > li > a').removeClass('active');
+        $(this).addClass('active');
+      }
+        else
+        {
+            $('#side-bar > ul > li > a').removeClass('active');
+            $('#side-bar > ul > li > ul').slideDown(400);
+        }
 
 }) ;
 }) ;
