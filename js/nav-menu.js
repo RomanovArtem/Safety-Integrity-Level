7a1d5dc5a2054416cb47b3fd29a1077366d9af62
Romanov Artem <temik_romanov@mail.ru>
2016-07-06 13:22:28 +0300

    Начал делать плавное появление левого меню.

---
+++ js/nav-menu.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+	$('#side-bar > ul > li > a').click(function(){
+    
+       // alert("Рад видеть Вас на моем сайте! Пошли дальше?");
+        // if($(this).attr('class') != 'active'){
+        $('#side-bar > ul > li > ul').slideDown(400);
+        //$(this).next().slideToggle(400); 
+        //$('#side-bar > ul > li > a').removeClass('active');
+        //$(this).addClass('active');}
+       // else{
+        //    $('#side-bar > ul > li > a').removeClass('active');
+        //    $('#side-bar > ul > li > ul').slideDown(400);
+       // }
+
+}) ;
+}) ;
+
+//}); старый торг, сквер с фонтаном и желтое здание , 
