53305bdcf26eb7ed82c86207133a54229c646f64
RomanovArtem <temik_romanov@mail.ru>
2016-07-07 14:42:51 +0400

    1) Сделал слайд шоу (карусель). 2) При наведении на картинки, они меняют цвет.

--- js/nav-menu.js
+++ js/nav-menu.js
@@ -14,8 +14,36 @@ $(document).ready(function() {
       $('#aaa').slideDown(400);
       $('#side-bar > ul > li > ul').slideToggle(400); 
     }
+});
+
+  $('#slider').jCarouselLite({
+    vertical: false,
+    hoverPause:true,
+    btnPrev: '#prev-img',
+    btnNext: "#next-img",
+    visible: 6,
+    speed: 500
+  });
+
+
+
+
+/*jQuery('#slider').jCarouselLite({
+    vertical: false,
+    btnNext: '#next-img',
+    btnPrev: '#prev-img',
+    visible: 6,
+    mouseWheel: true
+  });*/
+
+
+
+
 }) ;
-}) ;
+
+
+
+
 
 /// изменение цвета логотипа
 
