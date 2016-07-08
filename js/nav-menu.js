4cc22f52b2f113a38e1091ca238d4579b1776e96
Fadil Mamedov <fadil.mamedov@mail.ru>
2016-07-08 04:28:33 +0300

    Использование идентификаторов заменено на использование классов

--- js/nav-menu.js
+++ js/nav-menu.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
-    $('#low-voltage-systems').click(function() {
-        if($('#low-voltage-systems').attr('class') != 'active') {
-            $('#side-bar > ul > li > ul').slideDown(400);
-            $('#low-voltage-systems').removeClass('active');
+    $('.low-voltage-systems').click(function() {
+        if($('.low-voltage-systems').attr('class') != 'active') {
+            $('.side-bar > ul > li > ul').slideDown(400);
+            $('.low-voltage-systems').removeClass('active');
             $(this).addClass('active');
         } else {
-            $('side-bar > ul > li > ul').removeClass('active');
-            $('#low-voltage-systems').slideDown(400);
-            $('#side-bar > ul > li > ul').slideToggle(400);
+            $('.side-bar > ul > li > ul').removeClass('active');
+            $('.low-voltage-systems').slideDown(400);
+            $('.side-bar > ul > li > ul').slideToggle(400);
         }
     });
 
-    $('#slider').jCarouselLite({
+    $('.slider').jCarouselLite({
         vertical: false,
         hoverPause: true,
         btnPrev: '#prev-img',
@@ -20,11 +20,11 @@ $(document).ready(function() {
         speed: 500
     });
 
-    $('#feedback').click(function() {
-        $('#floating-block').slideToggle(400);
+    $('.feedback').click(function() {
+        $('.floating-block').slideToggle(400);
     });
 
-    $('#floating-block').click(function() {
-        $('#floating-block').slideToggle(400)
+    $('.floating-block').click(function() {
+        $('.floating-block').slideToggle(400)
     });
 });
